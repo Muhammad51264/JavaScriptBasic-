a6c4a7eb88d406e3f4aba5998e1f8d679b50e71c
Muhammad51264 <muh.freijat@gmail.com>
2023-04-26 12:48:02 +0300

    task

---
+++ Test.js
@@ -0,0 +1,49 @@
+console.log(".....................task1...................")
+let cash=1000,current_liabilities=500;
+console.log(cash/current_liabilities);
+console.log(cash-current_liabilities);
+console.log(cash+current_liabilities);
+console.log(cash*current_liabilities);
+let numbers=[7,9,2]
+console.log((numbers[0]+numbers[1]+numbers[2])/3);
+let price =150 ,discount=0.30
+console.log(price-price*discount);
+let age=20
+console.log(age>=18 && age<=30);
+let number1=2,number2=3
+console.log(number1**number2)
+number1=10,number2=4
+console.log(number1%number2)
+console.log(".....................task2...................")
+console.log(typeof(100))
+console.log(typeof(73.9))
+console.log(typeof(Nan))
+console.log(typeof("Water"))
+console.log(typeof(false))
+console.log(typeof(9!=11))
+console.log("Orang"+"e")
+console.log("Orange"-"s")
+console.log("4"+"8")
+console.log("4"-"8")
+console.log("name"+3)
+console.log("name"-"3")
+console.log(82*"word")
+console.log(1+"hello")
+console.log("hello"+1)
+console.log(1+true)
+console.log("hello"+true)
+console.log(typeof(Infinity))
+console.log(1=='1')
+console.log(1==='1')
+console.log(".....................task3...................")
+
+let text="Welcome to Orange"
+console.log(text.toUpperCase())
+console.log(text.slice(8, 11).toUpperCase())
+console.log(text.replace("Welcome","Hello"))
+console.log(text.toLowerCase())
+console.log(text.length)
+console.log(text.replace("Orange",'"Orange"'))
+console.log(text + " "+"Jordan")
+let example="cactus"
+console.log("c"+example.slice(1).replace(/c/g,"*"))
